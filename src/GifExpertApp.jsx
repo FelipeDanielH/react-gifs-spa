cd10b9a1593172a294fcc05806249959fd35f203
Felipe <felipe.daniel.henriquez@gmail.com>
2022-11-15 08:40:18 -0300

    primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState([]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  }
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory
+        onNewCategory={onAddCategory}
+        currentCategories={categories}
+      />
+
+      <ol>
+        {
+          categories.map(category => (
+            <GifGrid
+              key={category}
+              category={category}
+            />
+          ))
+        }
+      </ol>
+
+    </>
+  )
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = ({ target }) => {
+        setInputValue(target.value);
+    }
+
+    const onSubmit = (event) =>{
+        event.preventDefault();
+        if(inputValue.trim().length < 1) return;
+
+        setInputValue('');
+        onNewCategory( inputValue.trim());
+    }
+
+    return (
+        <form onSubmit={ onSubmit }>
+            <input
+                type="text"
+                placeholder="buscar gif"
+
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+    )
+}
