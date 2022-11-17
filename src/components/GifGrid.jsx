cd92eacabffa0dd5ca1c24555efbfb62cc9bc804
Felipe <felipe.daniel.henriquez@gmail.com>
2022-11-17 17:00:17 -0300

    se agregan proptypes para testing

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { GifItem } from './GifItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
+import PropTypes from 'prop-types';
 
 export const GifGrid = ({ category }) => {
 
@@ -26,4 +27,8 @@ export const GifGrid = ({ category }) => {
             </div>
         </>
     )
+}
+
+GifGrid.propTypes = {
+    category: PropTypes.string.isRequired,
 }
