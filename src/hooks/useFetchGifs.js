dc10575bfb05d5e7206fa591f6afe32bbf1a37ed
Felipe <felipe.daniel.henriquez@gmail.com>
2022-11-17 17:02:06 -0300

    se formatea el codigo

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react';
-import { getGifs } from '../helpers/getGifs';
+import { GetGifs } from '../helpers/GetGifs';
 
-export const useFetchGifs = ( category ) => {
+export const useFetchGifs = (category) => {
 
     const [images, setimages] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
     const getImages =
         async () => {
-            const newImages = await getGifs(category);
+            const newImages = await GetGifs(category);
             setimages(newImages);
             setIsLoading(false);
         }
 
     useEffect(() => {
         getImages();
-    }, []) 
+    }, [])
 
     return {
         images,
