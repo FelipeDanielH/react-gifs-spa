2c5a9c22cc352af0a6e48087aefccb036650f3d8
Felipe <felipe.daniel.henriquez@gmail.com>
2022-11-17 16:59:46 -0300

    se agregan proptypes para testing

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
 
@@ -17,7 +18,7 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     return (
-        <form onSubmit={ onSubmit }>
+        <form onSubmit={ onSubmit } aria-label="form">
             <input
                 type="text"
                 placeholder="buscar gif"
@@ -27,4 +28,9 @@ export const AddCategory = ({ onNewCategory }) => {
             />
         </form>
     )
+}
+
+
+AddCategory.propTypes = {
+    onNewCategory: PropTypes.func.isRequired,
 }
