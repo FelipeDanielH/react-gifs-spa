7bad57af241d908c310b6118911bda7207c558c0
Felipe <felipe.daniel.henriquez@gmail.com>
2022-11-17 17:06:29 -0300

    se formatea el codigo

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,14 +1,14 @@
-export const getGifs = async (category) =>{
+export const GetGifs = async (category) => {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=38ZCounWHLKaHf2l03vUHP7dgedVvizJ&q=${category}&limit=10`;
     const resp = await fetch(url);
 
-    const {data} = await resp.json();
+    const { data } = await resp.json();
 
-    const gifs = data.map( img => ({
+    const gifs = data.map(img => ({
         id: img.id,
-        title:img.title,
+        title: img.title,
         url: img.images.downsized_medium.url
-    }))
+    }));
 
     return gifs
 }
